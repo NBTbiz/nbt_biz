1178845edbe24d7ad79c83059af0f84239bb9df0
Imad Ibrahim <imadibrahim164@gmail.com>
2024-01-19 12:31:45 +0530

    changed number

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -8,7 +8,7 @@ export default function ContactInfoWidget({withIcon, title}) {
       <ul className="cs-menu_widget cs-style1 cs-mp0">
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="material-symbols:add-call-rounded" /></span>:''}
-          +91 9497702242
+          +91 80753 15549
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:envelope" /></span>:''}
